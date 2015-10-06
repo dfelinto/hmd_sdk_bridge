ec5d662f6901f641e3b6186282cf2e5d4bd47329
Dalai Felinto <dfelinto@gmail.com>
2015-10-06 12:40:00 -0300

    fixup batch fix

--- tests/run-test.bat
+++ tests/run-test.bat
@@ -7,7 +7,7 @@ REM copy wrapper
 mkdir bridge
 copy ..\wrapper\*.py bridge\
 mkdir bridge\win32-x86
-cp ..\Release\*.dll bridge\win32-x86
+copy ..\Release\*.dll bridge\win32-x86\
 
 REM run test
 C:\python34\python.exe run-test.py
