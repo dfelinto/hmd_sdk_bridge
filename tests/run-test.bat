f7cab8349e0a47cabaf47e6a387c3ad979cb4181
Dalai Felinto <dfelinto@gmail.com>
2015-10-06 12:29:30 -0300

    Fix test batch

--- tests/run-test.bat
+++ tests/run-test.bat
@@ -1,12 +1,13 @@
 
 REM cleanup deployment folder
-rm -rf bridge
+
+rd /Q /S bridge
 
 REM copy wrapper
 mkdir bridge
-cp ../wrapper/*.py bridge/
+copy ..\wrapper\*.py bridge\
 mkdir bridge\win32-x86
-cp ../Release/*.dll bridge/win32-x86
+cp ..\Release\*.dll bridge\win32-x86
 
 REM run test
 C:\python34\python.exe run-test.py
