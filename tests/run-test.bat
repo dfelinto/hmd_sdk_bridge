fb0f8e2fb8bd37782db97e7ba9c195938279eb0a
Dalai Felinto <dfelinto@gmail.com>
2015-10-06 15:43:40 -0300

    Prepare x64 as well

--- tests/run-test.bat
+++ tests/run-test.bat
@@ -8,6 +8,8 @@ mkdir bridge
 copy ..\wrapper\*.py bridge\
 mkdir bridge\win32-x86
 copy ..\Release\*.dll bridge\win32-x86\
+mkdir bridge\win32-x86-64
+copy ..\x64\Release\*.dll bridge\win32-x86-64\
 
 REM run test
 C:\python34\python.exe run-test.py
