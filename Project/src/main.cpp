de264f99cbcf8db2e26103e0cb2917237b2b7199
Dalai Felinto <dfelinto@gmail.com>
2015-10-06 11:34:37 -0300

    Test function to test the wrapper

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -2,6 +2,19 @@
 #include "Oculus.h"
 #include "../Include/Bridge_C_API.h"
 
+#include <iostream>
+
+extern "C"
+{
+	int functionTest(int input);
+}
+
+int functionTest(int input)
+{
+	std::cout << "functionTest: " << input << std::endl;
+	return input * 2;
+}
+
 int main()
 {
 	return 0;
