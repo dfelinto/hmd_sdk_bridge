56661eade7fe1052ca93e161b9a089a730e04c1a
Dalai Felinto <dfelinto@gmail.com>
2015-10-20 10:39:45 -0200

    Debug opengl code

--- Project/src/Debug.h
+++ Project/src/Debug.h
@@ -3,6 +3,9 @@
 
 #include <iostream>
 
+#include <GL/CAPI_GLE.h>
+#include "OVR_CAPI_GL.h"
+
 class Debug
 {
 public:
@@ -32,6 +35,34 @@ public:
 	{
 		/* TODO draw something in the texture*/
 		printf("debug::draw(%d)\n", color_texture);
+#if 0
+		Ideally we want to:
+			* create a framebuffer
+			* bind the color_texture to it
+			* bind the framebuffer
+			* draw something
+
+			But for now I am happy if we
+#endif
+		GLuint fbo[2];
+
+		printf("%s : %d\n", __FILE__, __LINE__);
+		glGenFramebuffers(1, fbo); /* it crashes here at the moment */
+		printf("%s : %d\n", __FILE__, __LINE__);
+
+#if 1
+		GLuint texName[2];
+		glGenTextures(1, texName);
+		printf("%s : %d\n", __FILE__, __LINE__);
+		glActiveTexture(GL_TEXTURE0);
+		printf("%s : %d\n", __FILE__, __LINE__);
+		glBindTexture(GL_TEXTURE_2D, texName[0]);
+		printf("%s : %d\n", __FILE__, __LINE__);
+		glCopyTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 0, 0, 128, 128, 0);
+		printf("%s : %d\n", __FILE__, __LINE__);
+		glBindTexture(GL_TEXTURE_2D, 0);
+		printf("%s : %d\n", __FILE__, __LINE__);
+#endif
 	};
 
 	static int multiplicationFactor() { return m_factor; };
