75845e35f78217053e188f1fbd191ca23c714ef5
Dalai Felinto <dfelinto@gmail.com>
2016-04-14 13:53:05 -0300

    Roll back part of the C_API changes for backward compatibility

--- Project/Include/Bridge_C_API.h
+++ Project/Include/Bridge_C_API.h
@@ -41,20 +41,20 @@ EXPORT_LIB void HMD_scaleSet(HMD *hmd, const float scale) { hmd->setScale(scale)
 
 #ifdef OCULUS
 /* Oculus wrapper - kept for backward compatibility */
-EXPORT_LIB HMD *Oculus_new(){ return new HMD(HMD::eHMDBackend::BACKEND_OCULUS); }
-EXPORT_LIB void Oculus_del(HMD *hmd){ if (hmd) delete hmd; }
-EXPORT_LIB bool Oculus_setup(HMD *hmd, const unsigned int color_texture_left, const unsigned int color_texture_right){ return hmd->setup(color_texture_left, color_texture_right); }
-EXPORT_LIB bool Oculus_update(HMD *hmd, float *r_orientation_left, float *r_position_left, float *r_orientation_right, float *r_position_right){ return hmd->update(r_orientation_left, r_position_left, r_orientation_right, r_position_right); };
-EXPORT_LIB bool Oculus_frameReady(HMD *hmd) { return hmd->frameReady(); }
-EXPORT_LIB bool Oculus_reCenter(HMD *hmd) { return hmd->reCenter(); }
-EXPORT_LIB unsigned int Oculus_widthLeft(HMD *hmd) { return hmd->getWidthLeft(); }
-EXPORT_LIB unsigned int Oculus_heightLeft(HMD *hmd) { return hmd->getHeightLeft(); }
-EXPORT_LIB unsigned int Oculus_widthRight(HMD *hmd) { return hmd->getWidthRight(); }
-EXPORT_LIB unsigned int Oculus_heightRight(HMD *hmd) { return hmd->getHeightRight(); }
-EXPORT_LIB void Oculus_projectionMatrixLeft(HMD *hmd, const float nearz, const float farz, float *r_matrix) { hmd->getProjectionMatrixLeft(nearz, farz, true, true, r_matrix); }
-EXPORT_LIB void Oculus_projectionMatrixRight(HMD *hmd, const float nearz, const float farz, float *r_matrix) { hmd->getProjectionMatrixRight(nearz, farz, true, true, r_matrix); }
-EXPORT_LIB float Oculus_scaleGet(HMD *hmd) { return hmd->getScale(); }
-EXPORT_LIB void Oculus_scaleSet(HMD *hmd, const float scale) { hmd->setScale(scale); }
+EXPORT_LIB Oculus *Oculus_new(){ return new Oculus(); }
+EXPORT_LIB void Oculus_del(Oculus *oculus){ if (oculus) delete oculus; }
+EXPORT_LIB bool Oculus_setup(Oculus *oculus, const unsigned int color_texture_left, const unsigned int color_texture_right){ return oculus->setup(color_texture_left, color_texture_right); }
+EXPORT_LIB bool Oculus_update(Oculus *oculus, float *r_orientation_left, float *r_position_left, float *r_orientation_right, float *r_position_right){ return oculus->update(r_orientation_left, r_position_left, r_orientation_right, r_position_right); };
+EXPORT_LIB bool Oculus_frameReady(Oculus *oculus) { return oculus->frameReady(); }
+EXPORT_LIB bool Oculus_reCenter(Oculus *oculus) { return oculus->reCenter(); }
+EXPORT_LIB unsigned int Oculus_widthLeft(Oculus *oculus) { return oculus->getWidthLeft(); }
+EXPORT_LIB unsigned int Oculus_heightLeft(Oculus *oculus) { return oculus->getHeightLeft(); }
+EXPORT_LIB unsigned int Oculus_widthRight(Oculus *oculus) { return oculus->getWidthRight(); }
+EXPORT_LIB unsigned int Oculus_heightRight(Oculus *oculus) { return oculus->getHeightRight(); }
+EXPORT_LIB void Oculus_projectionMatrixLeft(Oculus *oculus, const float nearz, const float farz, float *r_matrix) { oculus->getProjectionMatrixLeft(nearz, farz, true, true, r_matrix); }
+EXPORT_LIB void Oculus_projectionMatrixRight(Oculus *oculus, const float nearz, const float farz, float *r_matrix) { oculus->getProjectionMatrixRight(nearz, farz, true, true, r_matrix); }
+EXPORT_LIB float Oculus_scaleGet(Oculus *oculus) { return oculus->getScale(); }
+EXPORT_LIB void Oculus_scaleSet(Oculus *oculus, const float scale) { oculus->setScale(scale); }
 #endif
 
 #undef EXPORT_LIB
