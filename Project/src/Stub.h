5dcebcf2b14b2f820d1a67901677b7fb8198b63a
Dalai Felinto <dfelinto@gmail.com>
2016-04-19 10:09:50 -0300

    Remove building warnings for Stub

--- Project/src/Stub.h
+++ Project/src/Stub.h
@@ -19,25 +19,25 @@ public:
 	Stub();
 	~Stub();
 
-	bool setup(const unsigned int color_texture_left, const unsigned int color_texture_right) { return false;  }
+	bool setup(const unsigned int, const unsigned int) { return false;  }
 
-	bool update(float *r_orientation_left, float *r_position_left, float *r_orientation_right, float *r_position_right) { return false; }
+	bool update(float *, float *, float *, float *) { return false; }
 
 	bool update(
-		float *r_yaw_left, float *r_pitch_left, float *r_roll_left, float *r_position_left,
-		float *r_yaw_right, float *r_pitch_right, float *r_roll_right, float *r_position_right)
+		float *, float *, float *, float *,
+		float *, float *, float *, float *)
 	{
 		return false;
 	}
 
 	bool update(
-		float *r_yaw_left, float *r_pitch_left, float *r_roll_left, float *r_orientation_left, float *r_position_left,
-		float *r_yaw_right, float *r_pitch_right, float *r_roll_right, float *r_orientation_right, float *r_position_right)
+		float *, float *, float *, float *, float *,
+		float *, float *, float *, float *, float *)
 	{
 		return false;
 	}
 
-	bool update(float *r_matrix_left, float *r_matrix_right)
+	bool update(float *, float *)
 	{
 		return false;
 	}
@@ -52,9 +52,9 @@ public:
 		return false;
 	}
 
-	void getProjectionMatrixLeft(const float nearz, const float farz, const bool is_opengl, const bool is_right_hand, float *r_matrix) {}
+	void getProjectionMatrixLeft(const float, const float, const bool, const bool, float *) {}
 
-	void getProjectionMatrixRight(const float nearz, const float farz, const bool is_opengl, const bool is_right_hand, float *r_matrix) {}
+	void getProjectionMatrixRight(const float, const float, const bool, const bool, float *) {}
 };
 
 #endif /* __STUB_H__ */
