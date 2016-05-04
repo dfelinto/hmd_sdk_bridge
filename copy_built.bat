0644cbc6b25a67bfc618d3740a52159dfd565440
William Culver <bill@cedeon.co.uk>
2016-05-04 10:38:17 +0100

    Added some post-build automation

--- copy_built.bat
+++ copy_built.bat
@@ -1,2 +1,5 @@
-cp build\Win32\Release\BridgeLib.dll bridge\dylibs\win32-x86
-cp build\x64\Release\BridgeLib.dll bridge\dylibs\win32-x86-64
+REM ~~~ Put this in the solution as a post build script ~~~
+cd %1
+cp .\build\Win32\Release\BridgeLib.dll bridge\dylibs\win32-x86
+cp .\build\x64\Release\BridgeLib.dll bridge\dylibs\win32-x86-64
+exit 0
---
+++ copy_inputs.bat
@@ -0,0 +1,2 @@
+cp .\input\win32-x86\OculusVR.dll .\bridge\dylibs\win32-x86\OculusVR.dll
+cp .\input\win32-x86-64\OculusVR.dll .\bridge\dylibs\win32-x86-64\OculusVR.dll 
