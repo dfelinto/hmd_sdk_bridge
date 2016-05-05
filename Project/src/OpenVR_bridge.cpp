cb18607d82741c8a7d30e8953b38b1669b37f8ba
William Culver <bill@cedeon.co.uk>
2016-05-05 17:29:00 +0100

    Added some implementation to OpenVR

--- Project/src/OpenVR_bridge.cpp
+++ Project/src/OpenVR_bridge.cpp
@@ -19,8 +19,6 @@
 
 // using namespace vr;
 
-
-
 typedef enum eLibStatus
 {
 	LIB_UNLOADED = 0,
@@ -64,13 +62,20 @@ private:
 	bool isConnected(void);
 	unsigned int getProjectionMatrixFlags(const bool is_opengl, const bool is_right_hand);
 	bool initializeLibrary(void);
+	bool initializeCompositor(void);
+	Matrix4 ConvertSteamVRMatrixToMatrix4(const vr::HmdMatrix34_t &matPose);
+	void UpdateHMDMatrixPose();
 
 	unsigned int m_frame;
-
 	vr::IVRSystem *m_pHMDy;
-
 	static eLibStatus m_lib_status;
+	Matrix4 m_rMat4TrackedPose[vr::k_unMaxTrackedDeviceCount];
+	Matrix4 m_rmat4DevicePose[ vr::k_unMaxTrackedDeviceCount ];
+	Matrix4 m_mat4HMDPose;
+	char m_rDevClassChar[vr::k_unMaxTrackedDeviceCount];   // for each device, a character representing its class
+	vr::TrackedDevicePose_t m_rTrackedDevicePose[vr::k_unMaxTrackedDeviceCount];
 	GLuint m_fbo[2];
+
 };
 
 eLibStatus OpenVRImpl::m_lib_status = LIB_UNLOADED;
@@ -110,7 +115,69 @@ bool OpenVRImpl::initializeLibrary()
 	return false;
 }
 
-OpenVRImpl::OpenVRImpl() :Backend()
+bool OpenVRImpl::initializeCompositor() 
+{
+	vr::EVRInitError peError = vr::VRInitError_None;
+
+	if (!vr::VRCompositor())
+	{
+		return false;
+	}
+	return true;
+}
+
+Matrix4 OpenVRImpl::ConvertSteamVRMatrixToMatrix4(const vr::HmdMatrix34_t &matPose)
+{
+	Matrix4 matrixObj(
+		matPose.m[0][0], matPose.m[1][0], matPose.m[2][0], 0.0,
+		matPose.m[0][1], matPose.m[1][1], matPose.m[2][1], 0.0,
+		matPose.m[0][2], matPose.m[1][2], matPose.m[2][2], 0.0,
+		matPose.m[0][3], matPose.m[1][3], matPose.m[2][3], 1.0f
+		);
+	return matrixObj;
+}
+
+void OpenVRImpl::UpdateHMDMatrixPose()
+{
+	if (!m_pHMDy)
+		return;
+
+	vr::VRCompositor()->WaitGetPoses(m_rTrackedDevicePose, vr::k_unMaxTrackedDeviceCount, NULL, 0);
+
+	int m_iValidPoseCount = 0;
+	std::string m_strPoseClasses = "";
+	
+
+	for (int nDevice = 0; nDevice < vr::k_unMaxTrackedDeviceCount; ++nDevice)
+	{
+		if (m_rTrackedDevicePose[nDevice].bPoseIsValid)
+		{
+			m_iValidPoseCount++;
+			m_rmat4DevicePose[nDevice] = ConvertSteamVRMatrixToMatrix4(m_rTrackedDevicePose[nDevice].mDeviceToAbsoluteTracking);
+			if (m_rDevClassChar[nDevice] == 0)
+			{
+				switch (m_pHMDy->GetTrackedDeviceClass(nDevice))
+				{
+				case vr::TrackedDeviceClass_Controller:        m_rDevClassChar[nDevice] = 'C'; break;
+				case vr::TrackedDeviceClass_HMD:               m_rDevClassChar[nDevice] = 'H'; break;
+				case vr::TrackedDeviceClass_Invalid:           m_rDevClassChar[nDevice] = 'I'; break;
+				case vr::TrackedDeviceClass_Other:             m_rDevClassChar[nDevice] = 'O'; break;
+				case vr::TrackedDeviceClass_TrackingReference: m_rDevClassChar[nDevice] = 'T'; break;
+				default:                                       m_rDevClassChar[nDevice] = '?'; break;
+				}
+			}
+			m_strPoseClasses += m_rDevClassChar[nDevice];
+		}
+	}
+
+	if (m_rTrackedDevicePose[vr::k_unTrackedDeviceIndex_Hmd].bPoseIsValid)
+	{
+		m_mat4HMDPose = m_rmat4DevicePose[vr::k_unTrackedDeviceIndex_Hmd].invert();
+	}
+}
+
+OpenVRImpl::OpenVRImpl() 
+	:Backend()
 {
 	std::cout << "OpenVR()" << std::endl;
 
@@ -123,12 +190,16 @@ OpenVRImpl::OpenVRImpl() :Backend()
 	this->m_fbo[0] = 0;
 	this->m_fbo[1] = 0;
 
-	this->m_width[0] = 0;
-	this->m_width[1] = 0;
-	this->m_height[0] = 0;
-	this->m_height[1] = 0;
+	// ovr_GetFovTextureSize on a DK2 is 1182x1464 - hardcode for now.
+	this->m_width[0] = 1182;
+	this->m_width[1] = 1182;
+	this->m_height[0] = 1464;
+	this->m_height[1] = 1464;
 
-	this->m_status = "";
+	this->m_status = "Inside DLL - Constructor";
+
+	/* We set to true and then check some things.  Each failed step is to set this to false */
+	this->setStateBool(true);
 
 	/* Make sure the library is loaded */
 	if (this->initializeLibrary() == false) {
@@ -140,14 +211,22 @@ OpenVRImpl::OpenVRImpl() :Backend()
 	}
 
 
-	if (this->isConnected() == false) {
-		this->setStatus( "Error: OpenVR device not connected" );
+	if (this->initializeCompositor() == false) {
+		this->setStatus("Compositor initialization failed.\n");
 		this->setStateBool(false);
 
 		std::cout << this->getStatus() << std::endl;
 	}
 
-	std::cout << "OpenVR properly initialized (" << m_width[0] << "x" << m_height[0] << ", " << m_width[1] << "x" << m_height[1] << ")" << std::endl;
+	this->m_status = "After Init Library and Init Constructor";
+
+	if (this->getStateBool())
+	{
+		this->setStatus("OpenVR properly initialized.\n");
+		std::cout << "OpenVR properly initialized (" << m_width[0] << "x" << m_height[0] << ", " << m_width[1] << "x" << m_height[1] << ")" << std::endl;
+	}
+
+	
 }
 
 OpenVRImpl::~OpenVRImpl()
@@ -165,17 +244,23 @@ bool OpenVRImpl::isConnected()
 
 bool OpenVRImpl::setup(const unsigned int color_texture_left, const unsigned int color_texture_right)
 {
-	// Make eye render buffers
-	for (int eye = 0; eye < 2; eye++) {
+	this->m_color_texture[0] = color_texture_left;
+	this->m_color_texture[1] = color_texture_right;
 
+	for (int eye = 0; eye < 2; eye++) {
+		glGenFramebuffers(1, &this->m_fbo[eye]);
+		glBindFramebuffer(GL_READ_FRAMEBUFFER, this->m_fbo[eye]);
+		glFramebufferTexture2D(GL_READ_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_color_texture[eye], 0);
+		glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
 	}
 
-	std::cout << "OpenVR NOT properly setup." << std::endl;
+	std::cout << "OpenVR setup() inside BridgeLib.dll complete." << std::endl;
 	return true;
 };
 
 bool OpenVRImpl::update(float *r_orientation_left, float *r_position_left, float *r_orientation_right, float *r_position_right)
 {
+	// seems like this is the only one used.
 	return false;
 };
 
@@ -195,11 +280,23 @@ bool OpenVRImpl::update(
 
 bool OpenVRImpl::update(float *r_matrix_left, float *r_matrix_right)
 {
-	return false;
+	/*  Gets Tracking Data */
+	vr::TrackedDevicePose_t rTrackedDevicePose[vr::k_unMaxTrackedDeviceCount];
+
+	m_pHMDy->GetDeviceToAbsoluteTrackingPose(vr::TrackingUniverseSeated, 0.0f, 
+		VR_ARRAY_COUNT(vr::k_unMaxTrackedDeviceCount) rTrackedDevicePose, vr::k_unMaxTrackedDeviceCount);
+
+	for (uint32_t i = 0; i < vr::k_unMaxTrackedDeviceCount; i++) 
+	{
+		m_rMat4TrackedPose[i] = this->ConvertSteamVRMatrixToMatrix4(rTrackedDevicePose[i].mDeviceToAbsoluteTracking);
+	}
+	
+	return true;
 }
 
 bool OpenVRImpl::frameReady()
 {
+	/* Renders the Frame */
 	for (int eye = 0; eye < 2; eye++) {
 		
 	}
@@ -209,7 +306,9 @@ bool OpenVRImpl::frameReady()
 
 bool OpenVRImpl::reCenter()
 {
+	m_pHMDy->ResetSeatedZeroPose();
 	return true;
+
 };
 
 void OpenVRImpl::getProjectionMatrixLeft(const float nearz, const float farz, const bool is_opengl, const bool is_right_hand, float *r_matrix)
@@ -226,10 +325,6 @@ unsigned int OpenVRImpl::getProjectionMatrixFlags(const bool is_opengl, const bo
 {
 	return 0;
 }
-
-
-
-
 
 
 
