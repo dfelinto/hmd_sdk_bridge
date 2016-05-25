8b7a6938888bee41cdda4ee0c91f9faff2718eac
Dalai Felinto <dfelinto@gmail.com>
2016-05-25 17:12:54 +0000

    Integrate the API in a single header + a C++

--- source/HMD_Bridge_API.h
+++ source/HMD_Bridge_API.h
@@ -1,11 +1,12 @@
-#ifndef __BRIDGE_C_API_H__
-#define __BRIDGE_C_API_H__
-
-#include "Debug.h"
-
-#include "HMD.h"
+#ifndef __HMD_BRIDGE_API_H__
+#define __HMD_BRIDGE_API_H__
 
 #if defined(_WIN32) || defined(_WIN64)
+#if !defined(DllExport)
+	#define DllExport   __declspec( dllexport )  
+#endif
+
+/* C API */
 #if defined DLL_EXPORT
 #define EXPORT_LIB extern "C" __declspec(dllexport)
 #else
@@ -14,6 +15,62 @@
 #else
 #define EXPORT_LIB extern "C"
 #endif
+
+
+class DllExport HMD
+{
+public:
+
+  enum eHMDBackend
+	{
+		BACKEND_OCULUS = 0,
+		BACKEND_OCULUS_LEGACY,
+		BACKEND_VIVE,
+		BACKEND_OSVR,
+		BACKEND_OPENVR,
+		BACKEND_OPENHMD,
+	};
+
+	HMD(): m_hmd(new Oculus);
+
+	HMD(eHMDBackend backend);
+
+	~HMD(void);
+
+	bool setup(const unsigned int color_texture_left, const unsigned int color_texture_right);
+
+	bool update(float *r_orientation_left, float *r_position_left, float *r_orientation_right, float *r_position_right);
+
+	bool update(
+		float *r_yaw_left, float *r_pitch_left, float *r_roll_left, float *r_position_left,
+		float *r_yaw_right, float *r_pitch_right, float *r_roll_right, float *r_position_right);
+
+	bool update(
+		float *r_yaw_left, float *r_pitch_left, float *r_roll_left, float *r_orientation_left, float *r_position_left,
+		float *r_yaw_right, float *r_pitch_right, float *r_roll_right, float *r_orientation_right, float *r_position_right);
+
+	bool update(const bool is_right_hand, float *r_matrix_left, float *r_matrix_right);
+
+	bool frameReady(void);
+
+	bool reCenter(void);
+
+	void getProjectionMatrixLeft(const float nearz, const float farz, const bool is_opengl, const bool is_right_hand, float *r_matrix);
+
+	void getProjectionMatrixRight(const float nearz, const float farz, const bool is_opengl, const bool is_right_hand, float *r_matrix);
+
+	/* generic */
+	int getWidthLeft();
+	int getHeightLeft();
+	int getWidthRight();
+	int getHeightRight();
+	float getScale();
+	void setScale(const float scale);
+
+protected:
+	Backend *m_hmd;
+};
+
 
 /* Debug wrapper */
 EXPORT_LIB Debug *Debug_new(int number) { return new Debug(number); }
@@ -58,4 +115,5 @@ EXPORT_LIB void Oculus_scaleSet(HMD *hmd, const float scale) { hmd->setScale(sca
 #endif
 
 #undef EXPORT_LIB
-#endif /* __BRIDGE_C_API_H__ */
+
+#endif /* __HMD_BRIDGE_API_H__ */
