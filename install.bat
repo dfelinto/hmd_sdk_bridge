c13b7d2d996ede04aa27bc5b80dddc791ce02648
dfelinto <dfelinto@gmail.com>
2016-06-08 13:34:59 -0300

    Updated install script

--- install.bat
+++ install.bat
@@ -1,10 +1,12 @@
 REM copy addon
 
 SET BRIDGE="C:\src\hmd_sdk_bridge"
-SET RELEASE="C:\src\hmd_sdk_bridge_release"
+SET RELEASE="C:\src\hmd_sdk_bridge_cmake"
 
-SET RELEASE_32=%RELEASE%\Release
-SET RELEASE_64=%RELEASE%\x64\Release
+SET SHARED_32=%RELEASE%\shared_32\Release
+SET STATIC_32=%RELEASE%\static_32\Release
+SET SHARED_64=%RELEASE%\shared_64\Release
+SET STATIC_64=%RELEASE%\static_64\Release
 
 SET BUILD=%BRIDGE%\build
 cd %BUILD%
@@ -16,7 +18,7 @@ REM copy include file
 xcopy /Q /Y /E ..\source\HMD_Bridge_API.h include\
 
 REM copy compiled libraries
-xcopy /Q /Y /E %RELEASE_32%\BridgeLib.dll lib\x86\
-xcopy /Q /Y /E %RELEASE_32%\BridgeLib.lib lib\x86\
-xcopy /Q /Y /E %RELEASE_64%\BridgeLib.dll lib\x64\
-xcopy /Q /Y /E %RELEASE_64%\BridgeLib.lib lib\x64\
+xcopy /Q /Y /E %SHARED_32%\BridgeLib.dll lib\x86\
+xcopy /Q /Y /E %SHARED_64%\BridgeLib.dll lib\x64\
+xcopy /Q /Y /E %STATIC_32%\BridgeLib.lib lib\x86\
+xcopy /Q /Y /E %STATIC_64%\BridgeLib.lib lib\x64\
