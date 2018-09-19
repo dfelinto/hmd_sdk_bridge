6d5f918b0c7880aa180e0988dc5e1f39bc3aed4a
Dalai Felinto <dfelinto@gmail.com>
2018-09-19 20:48:43 +0000

    vive backend

---
+++ python/bridge/hmd/vive.py
@@ -0,0 +1,14 @@
+"""
+Vive
+======
+
+HTC Vive (vive.com) head mounted display for Windows
+"""
+
+from .backend import HMD as baseHMD
+from .backend import Backend
+
+
+class HMD(baseHMD):
+    _backend = Backend.VIVE
+
